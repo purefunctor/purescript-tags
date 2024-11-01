b1f741b79b7079c1d636f899be4f7b96f2ce4ce5
Justin Garcia <purefunctor@gmail.com>
2024-11-01 22:07:55 +0800

    Convert license.js to ESM

--- license.js
+++ license.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
-const glob = require("glob");
-const fs = require("fs");
-const path = require("path");
-const stream = require("stream");
-const MultiStream = require("multistream");
+import glob from "glob";
+import { createWriteStream, createReadStream } from "fs";
+import { sep } from "path";
+import { Readable } from "stream";
+import MultiStream from "multistream";
 
-let output = fs.createWriteStream("./dist/LICENSE-purs", { flags: "w+" });
+let output = createWriteStream("./dist/LICENSE-purs", { flags: "w+" });
 let licenses = (() => {
   let files = glob.sync(".spago/**/LICENSE");
   let tail = files.pop();
 
   let copyLicense = (newline) => (license) => {
-    let [_s, _p, name, _l] = license.split(path.sep);
+    let [_s, _p, name, _l] = license.split(sep);
     return [
-      stream.Readable.from(`LICENSE - ${decodeURIComponent(name)}\n\n`),
-      fs.createReadStream(license, "utf-8"),
-      stream.Readable.from(newline ? "\n" : ""),
+      Readable.from(`LICENSE - ${decodeURIComponent(name)}\n\n`),
+      createReadStream(license, "utf-8"),
+      Readable.from(newline ? "\n" : ""),
     ];
   };
 
